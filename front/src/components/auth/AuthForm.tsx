04d9926c277d98e395187ec492b70f9b00f32e28
Kaichi Onodera <kaichi.onodera501@gmail.com>
2025-10-26 07:04:02 +0900

    add design and state

--- front/src/components/auth/AuthForm.tsx
+++ front/src/components/auth/AuthForm.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import react, { useState } from 'react';
+import React, { useState } from 'react';
 
 interface AuthFormProps {
   mode: 'login' | 'signup';
@@ -17,41 +16,68 @@ export const AuthForm: React.FC<AuthFormProps> = ({ mode, userType }) => {
   };
 
   return (
-    <div className="bg-white">
+    <div className="bg-gray-slate p-8 rounded-lg shadow-lg max-w-md mx-auto">
       <h2 className="text-center">
         {userType === 'admin' ? '管理者' : 'ユーザー'}
         {mode === 'login' ? 'ログイン' : 'サインアップ'}
       </h2>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="space-y-6">
         {/* サインアップ時のみ表示 */}
         {mode === 'signup' && (
-          <>
+          <div className="grid grid-cols-2 gap-4">
             <div>
-              <label>姓： </label>
-              <input type="text"></input>
+              <label className="block">姓</label>
+              <input
+                type="text"
+                value={lastName}
+                className="block px-2 py-1"
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              ></input>
             </div>
             <div>
-              <label>名前： </label>
-              <input type="text"></input>
+              <label className="block">名前</label>
+              <input
+                type="text"
+                value={firstName}
+                className="block px-2 py-1"
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              ></input>
             </div>
-          </>
+          </div>
         )}
 
         {/* メールアドレス */}
         <div>
-          <label className="mt-1 block">メールアドレス： </label>
-          <input type="text"></input>
+          <label> メールアドレス</label>
+          <input
+            type="email"
+            value={email}
+            className="block px-3 py-1"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          ></input>
         </div>
 
         {/* パスワード */}
         <div>
-          <label className="block">パスワード： </label>
-          <input type="text"></input>
+          <label>パスワード</label>
+          <input
+            type="password"
+            value={password}
+            className="block px-3 py-1"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          ></input>
         </div>
 
         {/* 送信ボタン */}
-        <button type="submit">
+        <button
+          type="submit"
+          className="bg-black rounded-md text-white py-3 px-4"
+        >
           {mode === 'login' ? 'ログイン' : 'サインアップ'}
         </button>
       </form>
