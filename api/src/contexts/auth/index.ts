bd68d1d9f7d8862da9922fc47c9001b76d599430
Kaichi Onodera <kaichi.onodera501@gmail.com>
2025-10-28 12:32:52 +0900

    ログアウト機能追加

--- api/src/contexts/auth/index.ts
+++ api/src/contexts/auth/index.ts
@@ -1,5 +1,6 @@
 import { UserLoginController } from './controllers/UserLoginController';
 import { AdminLoginController } from './controllers/AdminLoginController';
+import { LogoutController } from './controllers/LogoutController';
 import { LoginInteractor } from './interactors/LoginInteractor';
 import { UserRepository } from './infrastructures/repositories/UserRepository';
 import { PrismaClient } from '@prisma/client';
@@ -12,6 +13,7 @@ const userRepository = new UserRepository(prisma);
 const loginInteractor = new LoginInteractor(userRepository);
 const userLoginController = new UserLoginController(loginInteractor);
 const adminLoginController = new AdminLoginController(loginInteractor);
+const logoutController = new LogoutController();
 
 authRouter.post(
   '/user/login',
@@ -21,5 +23,6 @@ authRouter.post(
   '/admin/login',
   adminLoginController.execute.bind(adminLoginController),
 );
+authRouter.post('/logout', logoutController.execute.bind(logoutController));
 
 export { authRouter };
