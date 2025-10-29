c843a89994026ec6e00d41a3bef1e3b4d69c60ef
Kaichi Onodera <kaichi.onodera501@gmail.com>
2025-10-29 09:04:06 +0900

    他のフォームと記述の統一

--- front/src/pages/auth/UserSignup.tsx
+++ front/src/pages/auth/UserSignup.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import AuthForm from '../../components/auth/AuthForm';
 
 export const UserSignup: React.FC = () => {
-  return <AuthForm mode="signup" userType="user" />;
+  return (
+    <div>
+      <AuthForm mode="signup" userType="user" />;
+    </div>
+  );
 };
