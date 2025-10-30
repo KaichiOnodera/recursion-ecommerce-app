c685cbd582bf3265146e77d9075c56a193a95f2d
Kaichi Onodera <kaichi.onodera501@gmail.com>
2025-10-31 03:01:42 +0900

    Requestの定義をsharedに以降

--- api/src/contexts/auth/controllers/LoginController.ts
+++ api/src/contexts/auth/controllers/LoginController.ts
@@ -1,17 +1,12 @@
 import express from 'express';
 import { ILoginInteractor } from '../usecases/ILoginInteractor';
-import { PostRes } from '@shared/types/posts';
-
-interface LoginRequest {
-  email: string;
-  password: string;
-}
+import { PostRes, PostReq } from '@shared/types/posts';
 
 export class LoginController {
   constructor(private readonly loginInteractor: ILoginInteractor) {}
 
   async execute(
-    req: express.Request<null, PostRes['auth/login'], LoginRequest>,
+    req: express.Request<null, PostRes['auth/login'], PostReq['auth/login']>,
     res: express.Response<PostRes['auth/login'] | { message: string }>,
   ) {
     const { email, password } = req.body;
--- shared/types/posts.ts
+++ shared/types/posts.ts
@@ -1,5 +1,12 @@
 import { User } from "@shared/schemas/user"
 
+export type PostReq = {
+    'auth/login': {
+        email: string
+        password: string
+    }
+}
+
 export type PostRes = {
     'auth/login': {
         user: User
