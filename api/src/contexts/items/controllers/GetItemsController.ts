3c590c83c5bb44e3ff053f6cc47e7d22c8f8e7db
sasaki <takomannto1996@gmail.com>
2025-11-01 01:55:54 +0900

    フォーマット修正

--- api/src/contexts/items/controllers/GetItemsController.ts
+++ api/src/contexts/items/controllers/GetItemsController.ts
@@ -1,22 +1,25 @@
-import express from "express";
-import { GetRes } from "@shared/types/gets";
-import { IGetItemsInteractor } from "../usecases/IGetItemsInteractor";
+import express from 'express';
+import { GetRes } from '@shared/types/gets';
+import { IGetItemsInteractor } from '../usecases/IGetItemsInteractor';
 
 export class GetItemsController {
-    constructor(private readonly getItemsInteractor: IGetItemsInteractor) {}
+  constructor(private readonly getItemsInteractor: IGetItemsInteractor) {}
 
-    async execute(_req: express.Request<null>, res: express.Response<GetRes['/items']>) {
-        const items = await this.getItemsInteractor.execute();
-        
-        const responseItems = items.map(item => ({
-            id: item.id,
-            name: item.name,
-            description: item.description,
-            type: item.type,
-            createdAt: item.createdAt,
-            updatedAt: item.updatedAt
-        }));
-        
-        res.status(201).json({ items: responseItems });
-    }
+  async execute(
+    _req: express.Request<null>,
+    res: express.Response<GetRes['/items']>,
+  ) {
+    const items = await this.getItemsInteractor.execute();
+
+    const responseItems = items.map((item) => ({
+      id: item.id,
+      name: item.name,
+      description: item.description,
+      type: item.type,
+      createdAt: item.createdAt,
+      updatedAt: item.updatedAt,
+    }));
+
+    res.status(201).json({ items: responseItems });
+  }
 }
