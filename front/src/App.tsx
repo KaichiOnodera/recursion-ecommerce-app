c314c2901eb473a096de68211c5d711ea9dcbeb8
Kaichi Onodera <kaichi.onodera501@gmail.com>
2025-11-15 00:07:39 +0900

    商品削除画面追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -13,6 +13,7 @@ import { UserSignup } from './pages/auth/UserSignup';
 import { AdminLogin } from './pages/auth/AdminLogin';
 import { AdminSignup } from './pages/auth/AdminSignup';
 import { AdminProductList } from './pages/admin/AdminProductList';
+import { AdminProductDelete } from './pages/admin/AdminProductDelete';
 
 function App(): React.JSX.Element {
   return (
@@ -28,6 +29,10 @@ function App(): React.JSX.Element {
             <Route path="/auth/admin/login" element={<AdminLogin />} />
             <Route path="/auth/admin/signup" element={<AdminSignup />} />
             <Route path="/admin/products" element={<AdminProductList />} />
+            <Route
+              path="/admin/products/:id/delete"
+              element={<AdminProductDelete />}
+            />
           </Routes>
         </div>
       </div>
