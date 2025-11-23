0e086398b24171c2e36b03b659621c074720731e
1017Kosuke <1017hamasuke@gmail.com>
2025-11-23 14:09:23 +0800

    オートフォーマッタの適用

--- api/src/contexts/users/index.ts
+++ api/src/contexts/users/index.ts
@@ -1,24 +1,23 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
-import { UserRepository } from "./infrastructures/repositories/UserRepository";
-import { verifyAccessToken } from "src/middlewares/verifyAccesToken";
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { UserRepository } from './infrastructures/repositories/UserRepository';
+import { verifyAccessToken } from 'src/middlewares/verifyAccesToken';
 
-import { GetUserByIdInteractor } from "./interactors/GetUserByIdInteractor";
-import { CreateUserInteractor } from "./interactors/CreateUserInteractor";
-import { UpdateUserProfileInteractor } from "./interactors/UpdateUserProfileInteractor";
-import { DeleteUserInteractor } from "./interactors/DeleteUserInteractor";
+import { GetUserByIdInteractor } from './interactors/GetUserByIdInteractor';
+import { CreateUserInteractor } from './interactors/CreateUserInteractor';
+import { UpdateUserProfileInteractor } from './interactors/UpdateUserProfileInteractor';
+import { DeleteUserInteractor } from './interactors/DeleteUserInteractor';
 
-import { GetUserByIdController } from "./controllers/GetUserByIdController";
-import { CreateUserController } from "./controllers/CreateUserController";
-import { UpdateUserProfileController } from "./controllers/UpdateUserProfileController";
-import { DeleteUserController } from "./controllers/DeleteUserController";
+import { GetUserByIdController } from './controllers/GetUserByIdController';
+import { CreateUserController } from './controllers/CreateUserController';
+import { UpdateUserProfileController } from './controllers/UpdateUserProfileController';
+import { DeleteUserController } from './controllers/DeleteUserController';
 
 const UsersRouter = express.Router();
 
 // Core dependencies
 const prisma = new PrismaClient();
 const userRepository = new UserRepository(prisma);
-
 
 const getUserByIdInteractor = new GetUserByIdInteractor(userRepository);
 const createUserInteractor = new CreateUserInteractor(userRepository);
@@ -28,25 +27,26 @@ const deleteUserInteractor = new DeleteUserInteractor(userRepository);
 // Controllers
 const getUserByIdController = new GetUserByIdController(getUserByIdInteractor);
 const createUserController = new CreateUserController(createUserInteractor);
-const updateUserProfileController = new UpdateUserProfileController(updateUserInteractor);
+const updateUserProfileController = new UpdateUserProfileController(
+  updateUserInteractor,
+);
 const deleteUserController = new DeleteUserController(deleteUserInteractor);
 
 // Routes
-UsersRouter.get("/",
-    verifyAccessToken,(req, res) => {
-        getUserByIdController.execute(req, res);
-    }
+UsersRouter.get('/', verifyAccessToken, (req, res) => {
+  getUserByIdController.execute(req, res);
+});
+UsersRouter.post(
+  '/signup',
+  createUserController.execute.bind(createUserController),
 );
-UsersRouter.post("/signup", createUserController.execute.bind(createUserController));
-UsersRouter.put("/users/:id",
-    verifyAccessToken, (req, res) => {
-        updateUserProfileController.execute(req, res);
-    }
+UsersRouter.put('/users/:id', verifyAccessToken, (req, res) => {
+  updateUserProfileController.execute(req, res);
+});
+UsersRouter.delete(
+  '/:id',
+  verifyAccessToken,
+  deleteUserController.execute.bind(deleteUserController),
 );
-UsersRouter.delete("/:id",
-    verifyAccessToken,
-    deleteUserController.execute.bind(deleteUserController)
-);
-
 
 export { UsersRouter };
--- shared/schemas/user.ts
+++ shared/schemas/user.ts
@@ -1,5 +1,5 @@
 export type User = {
-    id: number;
-    email: string;
-    role: 'USER' | 'ADMIN';
-}
+  id: number;
+  email: string;
+  role: 'USER' | 'ADMIN';
+};
