2b79b31e54c25e9e7b6823e1452a2cd345a0f7b1
1017Kosuke <1017hamasuke@gmail.com>
2025-11-24 19:21:43 +0800

    GetUserByIdの削除

--- api/src/contexts/users/index.ts
+++ api/src/contexts/users/index.ts
@@ -3,12 +3,10 @@ import { PrismaClient } from "@prisma/client";
 import { UserRepository } from "./infrastructures/repositories/UserRepository";
 import { verifyAccessToken } from "src/middlewares/verifyAccesToken";
 
-import { GetUserByIdInteractor } from "./interactors/GetUserByIdInteractor";
 import { CreateUserInteractor } from "./interactors/CreateUserInteractor";
 import { UpdateUserProfileInteractor } from "./interactors/UpdateUserProfileInteractor";
 import { DeleteUserInteractor } from "./interactors/DeleteUserInteractor";
 
-import { GetUserByIdController } from "./controllers/GetUserByIdController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { UpdateUserProfileController } from "./controllers/UpdateUserProfileController";
 import { DeleteUserController } from "./controllers/DeleteUserController";
@@ -20,23 +18,16 @@ const prisma = new PrismaClient();
 const userRepository = new UserRepository(prisma);
 
 
-const getUserByIdInteractor = new GetUserByIdInteractor(userRepository);
 const createUserInteractor = new CreateUserInteractor(userRepository);
 const updateUserInteractor = new UpdateUserProfileInteractor(userRepository);
 const deleteUserInteractor = new DeleteUserInteractor(userRepository);
 
 // Controllers
-const getUserByIdController = new GetUserByIdController(getUserByIdInteractor);
 const createUserController = new CreateUserController(createUserInteractor);
 const updateUserProfileController = new UpdateUserProfileController(updateUserInteractor);
 const deleteUserController = new DeleteUserController(deleteUserInteractor);
 
 // Routes
-UsersRouter.get("/",
-    verifyAccessToken,(req, res) => {
-        getUserByIdController.execute(req, res);
-    }
-);
 UsersRouter.post("/signup", createUserController.execute.bind(createUserController));
 UsersRouter.put("/users/:id",
     verifyAccessToken, (req, res) => {
--- api/src/contexts/users/usecases/IGetUserByIdInteractor.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "../domains/entities/User";
-
-export interface IGetUserByIdInteractor {
-  execute(userId:number): Promise<User | null>;
-}
