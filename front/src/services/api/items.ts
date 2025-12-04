be658c2b4eab7b3c482df063da1e5bcced1c18b9
Kosuke Arii <168504313+1017Kosuke@users.noreply.github.com>
2025-12-04 19:50:11 +0800

    Apply suggestion from @sasacheese
    Co-authored-by: Tatsuya Sasaki <takomannto1996@gmail.com>

--- front/src/services/api/items.ts
+++ front/src/services/api/items.ts
@@ -58,6 +58,6 @@ export async function getAdminItem(id: number): Promise<ItemResponse> {
 }
 
 export async function getItem(id: number): Promise<ItemResponse> {
-  const response = await apiClient.get<<GetRes['items/:id']>(`/item/${id}`);
+  const response = await apiClient.get<GetRes['items/:id']>(`/item/${id}`);
   return response.data;
 }
