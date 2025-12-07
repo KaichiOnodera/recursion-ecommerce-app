b45268e2e04f20e7eb694a6f9d0fab979bc47d1a
Kaichi Onodera <kaichi.onodera501@gmail.com>
2025-12-08 07:46:58 +0900

    不要なコードの削除

--- shared/types/delete.ts
+++ shared/types/delete.ts
@@ -1,13 +1,7 @@
-import { CartItem } from '../schemas/cart';
-
 export type DeleteReq = {
   'admin/items/:id': Record<string, never>;
-  'cart/items/:itemId': Record<string, never>;
-  '/cart': Record<string, never>;
 };
 
 export type DeleteRes = {
   'admin/items/:id': { deleted: boolean };
-  'cart/items/:itemId': { items: CartItem[] };
-  '/cart': { items: CartItem[] };
 };
--- shared/types/patches.ts
+++ shared/types/patches.ts
@@ -1,5 +1,4 @@
 import { Item } from '../schemas/item';
-import { CartItem } from '../schemas/cart';
 
 export type PatchReq = {
   'admin/items/:id': {
@@ -8,16 +7,10 @@ export type PatchReq = {
     type?: number;
     price: number;
   };
-  'cart/items/:itemId': {
-    amount: number;
-  };
 };
 
 export type PatchRes = {
   'admin/items/:id': {
     item: Item;
-  };
-  'cart/items/:itemId': {
-    items: CartItem[];
   };
 };
