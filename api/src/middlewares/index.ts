d8d66b8434670b7a29a743970de0b73ba6b394e5
sasaki <takomannto1996@gmail.com>
2025-12-20 22:18:10 +0900

    optionalVerifyAccessTokenのミドルウェア実装

--- api/src/middlewares/index.ts
+++ api/src/middlewares/index.ts
@@ -1,4 +1,7 @@
-import { createVerifyAccessToken } from './verifyAccessToken';
+import {
+  createVerifyAccessToken,
+  createOptionalVerifyAccessToken,
+} from './verifyAccessToken';
 import { VerifyUserInteractor } from '../contexts/auth/interactors/VerifyUserInteractor';
 import { UserRepository } from '../contexts/auth/infrastructures/repositories/UserRepository';
 import { prisma } from '../libs/prisma';
@@ -7,6 +10,8 @@ const userRepository = new UserRepository(prisma);
 const verifyUserInteractor = new VerifyUserInteractor(userRepository);
 
 export const verifyAccessToken = createVerifyAccessToken(verifyUserInteractor);
+export const optionalVerifyAccessToken =
+  createOptionalVerifyAccessToken(verifyUserInteractor);
 
 export { verifyAdmin } from './verifyAdmin';
 export type { AuthenticatedRequest } from './verifyAccessToken';
