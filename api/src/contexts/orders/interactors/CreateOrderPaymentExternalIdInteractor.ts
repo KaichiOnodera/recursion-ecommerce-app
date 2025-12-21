57134de2e63433a404802431f66aca9881eb9bdf
Kaichi Onodera <kaichi.onodera501@gmail.com>
2025-12-22 04:20:24 +0900

    型を明示

--- api/src/contexts/orders/interactors/CreateOrderPaymentExternalIdInteractor.ts
+++ api/src/contexts/orders/interactors/CreateOrderPaymentExternalIdInteractor.ts
@@ -3,13 +3,16 @@ import {
   CreateOrderPaymentExternalIdParams,
 } from '../usecases/ICreateOrderPaymentExternalIdInteractor';
 import { IOrderRepository } from '../domains/repositories/IOrderRepository';
+import { OrderPaymentExternalId } from '../domains/entities/OrderPaymentExternalId';
 
 export class CreateOrderPaymentExternalIdInteractor
   implements ICreateOrderPaymentExternalIdInteractor
 {
   constructor(private readonly orderRepository: IOrderRepository) {}
 
-  async execute(params: CreateOrderPaymentExternalIdParams) {
+  async execute(
+    params: CreateOrderPaymentExternalIdParams,
+  ): Promise<OrderPaymentExternalId> {
     return await this.orderRepository.createPaymentExternalId({
       orderId: params.orderId,
       provider: params.provider,
