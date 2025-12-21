4d5890409c06dab0718eebe4db99566510700edb
Kaichi Onodera <kaichi.onodera501@gmail.com>
2025-12-21 18:07:35 +0900

    注文作成のAPI

---
+++ api/src/contexts/orders/domains/repositories/IOrderRepository.ts
@@ -0,0 +1,44 @@
+import { OrderStatus } from '@prisma/client';
+
+export interface CreateOrderData {
+  userId?: number;
+  lastName: string;
+  firstName: string;
+  email: string;
+  address: string;
+  totalPrice: number;
+  orderStatus?: OrderStatus;
+  orderItems: Array<{
+    itemId?: number;
+    itemName: string;
+    itemPrice: number;
+    amount: number;
+  }>;
+}
+
+export interface Order {
+  id: number;
+  userId: number | null;
+  lastName: string;
+  firstName: string;
+  email: string;
+  address: string;
+  totalPrice: number;
+  orderStatus: OrderStatus;
+  createdAt: Date;
+  updatedAt: Date;
+  orderItems: Array<{
+    id: number;
+    orderId: number;
+    itemId: number | null;
+    itemName: string;
+    itemPrice: number;
+    amount: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }>;
+}
+
+export interface IOrderRepository {
+  create(data: CreateOrderData): Promise<Order>;
+}
