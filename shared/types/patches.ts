4358cb68aeb8900bcd0796e21bd16c42a141408b
Kaichi Onodera <kaichi.onodera501@gmail.com>
2026-01-13 22:47:59 +0900

    エンドポイント追加

--- shared/types/patches.ts
+++ shared/types/patches.ts
@@ -1,6 +1,7 @@
 import { User } from '../schemas/user';
 import { AdminItem } from '../schemas/item';
 import { Tag } from '../schemas/tag';
+import { Wishlist } from '../schemas/wishlist';
 
 export type PatchReq = {
   '/admin/items/:id': {
@@ -20,6 +21,10 @@ export type PatchReq = {
   '/admin/tags/:id': {
     name: string;
   };
+  '/wishlist/:wishlistId': {
+    name?: string | null;
+    isPublic?: boolean;
+  };
 };
 
 export type PatchRes = {
@@ -31,5 +36,8 @@ export type PatchRes = {
   };
   '/admin/tags/:id': {
     tag: Tag;
+  };
+  '/wishlist/:wishlistId': {
+    wishlist: Wishlist;
   };
 };
