f48d2d840ec449569ca7435fabfbd41ef7e3748b
Kaichi Onodera <kaichi.onodera501@gmail.com>
2026-01-16 08:01:09 +0900

    既存の設計に合わせて、stripeProductIdとPriceIdのフィールドをItemsから分離

--- api/prisma/migrations/20260116073619_add_stripe_ids_to_items/migration.sql
+++ api/prisma/migrations/20260116073619_add_stripe_ids_to_items/migration.sql
@@ -1,6 +1,18 @@
--- AlterTable
-ALTER TABLE `Items` ADD COLUMN `stripeProductId` VARCHAR(191) NULL,
-    ADD COLUMN `stripePriceId` VARCHAR(191) NULL;
+-- CreateTable
+CREATE TABLE `ItemStripeMapping` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `itemId` INTEGER NOT NULL,
+    `stripeProductId` VARCHAR(191) NOT NULL,
+    `stripePriceId` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
--- CreateIndex
-CREATE UNIQUE INDEX `Items_stripeProductId_key` ON `Items`(`stripeProductId`);
+    UNIQUE INDEX `ItemStripeMapping_itemId_key`(`itemId`),
+    UNIQUE INDEX `ItemStripeMapping_stripeProductId_key`(`stripeProductId`),
+    INDEX `ItemStripeMapping_itemId_idx`(`itemId`),
+    INDEX `ItemStripeMapping_stripeProductId_idx`(`stripeProductId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ItemStripeMapping` ADD CONSTRAINT `ItemStripeMapping_itemId_fkey` FOREIGN KEY (`itemId`) REFERENCES `Items`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
